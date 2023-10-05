b92882b600d8a9df6f17c38a8817ef6781938cdc
Adam Vessey <adam@discoverygarden.ca>
2023-10-05 13:55:07 -0300

    Pull out tracker class to separate file.
    DrupalPractice coding standards don't (fully) account for anonymous
    classes.

--- tests/src/Unit/HandlerAttachmentTest.php
+++ tests/src/Unit/HandlerAttachmentTest.php
@@ -63,62 +63,7 @@ class HandlerAttachmentTest extends UnitTestCase {
   public function testAttachments(string $class, string $interface, array $set_values = []) {
     $type_mock = $this->getEntityTypeMock($class, $interface);
 
-    $tracker = new class($set_values)  {
-
-      /**
-       * The originally passed values.
-       *
-       * @var array[]
-       */
-      protected array $values;
-
-      /**
-       * The originally passed values, rekeyed according to the first parameter.
-       *
-       * @var array[]
-       */
-      protected array $hashed;
-
-      /**
-       * Values that have been seen and matched.
-       *
-       * @var array[]
-       */
-      protected array $tracked = [];
-
-      /**
-       * Constructor.
-       */
-      public function __construct(array $values) {
-        $this->values = $values;
-        $this->hashed = array_column($this->values, NULL, 0);
-      }
-
-      /**
-       * Helper; facilitate the assertion that all params have been consumed.
-       *
-       * @return bool
-       *   TRUE if all values passed in the constructor appear to have been
-       *   checked in ::matches(); otherwise, FALSE.
-       */
-      public function isFullyConsumed() : bool {
-        return !array_diff_key($this->hashed, $this->tracked);
-      }
-
-      /**
-       * Track if the given value matches a set of values expected.
-       *
-       * @param array $other
-       *   A set of parameters passed to the `::set()` call.
-       */
-      public function matches(array $other) {
-        $key = $other[0];
-        if ($this->hashed[$key] === $other) {
-          $this->tracked[$key] = $other;
-        }
-      }
-
-    };
+    $tracker = new HandlerAttachmentTracker($set_values);
 
     $type_mock->expects($this->any())
       ->method('set')
@@ -126,7 +71,7 @@ class HandlerAttachmentTest extends UnitTestCase {
         $tracker->matches($params);
         return $type_mock;
       });
-;
+
     [$class, 'attach']($type_mock);
 
     $this->assertTrue($tracker->isFullyConsumed());
